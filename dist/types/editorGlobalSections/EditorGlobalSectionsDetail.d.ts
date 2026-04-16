5a3f9e45368bd623241e8c2ea56a90e5277d4ef3
Tam Nguyen <tam26918@gmail.com>
2026-04-16 09:16:35 +0700

    fix(fe): reduce bundle size

--- dist/types/editorGlobalSections/EditorGlobalSectionsDetail.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ILayer } from "../utils/normalizeComponentLayers";
-export declare const EditorGlobalSectionsDetail: ({ layers, currentLayer, onClickLayer, onFocusLayer, }: {
-    layers: ILayer[];
-    currentLayer?: string;
-    onClickLayer?: (id: string, path: string, rootParentId?: string) => void;
-    onFocusLayer?: (layerId: string) => void;
-}) => React.JSX.Element[];
-//# sourceMappingURL=EditorGlobalSectionsDetail.d.ts.map
--- dist/types/utils/colors.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const getBrightnessColor: (hex: string) => "#ffffff" | "#000000";
-//# sourceMappingURL=colors.d.ts.map
